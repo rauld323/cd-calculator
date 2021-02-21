1a19a75073ec242a1d0ec8de5599366fc4583d1d
Raul <rauldavila323@gmail.com>
2021-02-21 17:25:25 +0100

    Still need to limit user input.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,124 @@
-let num1 = '';
-let num2 = '';
-let operator = '';
-let total = '';
+let calcTotal = 0;
+let userInput = "0";
+let previousOperator;
+let screen = document.querySelector(".screen");
+// const numMax = 8;
 
 
-
-function numberFunc(num){
-	{
-		if (num1 === ''){
-			 num1 = num;
-			}else{
-				num2 = num;}
+function checScreen(){
+	if(userInput.length < 8){
+		screen.innerText = userInput.split("").slice(0,3)
 	}
 }
 
-function operatorFunc(oper) {
+checScreen()
+
+function rerender() {
+	screen.innerText  = userInput;
+}
+
+// 1. Whenever anything is clicked on, the init() function will run.
+function init() {
+	document.querySelector(".calc-buttons").addEventListener("click", function (event) {
+		buttonClick(event.target.innerText);
+	});
+}
+
+init();
+
+
+//Step 2 buttonClick() checks if its not a number. This is where the click event is checked and sent to its corresponding function
+function buttonClick(value) {
+	if (isNaN(parseInt(value))) {
+		handleSymbol(value);
+	} else {
+		handleNumber(value);
+	}
+	rerender();
+}
+//Step 3 if userInput is empty, assign 0. If it it has a value add to the end "+= value"
+function handleNumber(value) {
+	if (userInput === "0") {
+		userInput = value;
+	} else{
+		userInput += value;
+	}
+
+
+	if (value < 2){
+		userInput = "error"
+	}
 }
 
 
+//Step 4 If it is a symbol and AC we clear everything. If it is an "=" we have total everything. It it is "arrow" we deletethe last input. If none of this applies, use the operators
+function handleSymbol(value) {
+	switch (value) {
+		case "AC":
+			userInput = "0";
+			calcTotal = 0;
+			break;
 
-$('button').on('click', function (e) {
-	 let btn = e.target.innerHTML; if (btn >= '0' && btn <= '9')
-	 { numberFunc(btn);
+		case "=":
+			if (previousOperator === null) {
+				return;
+			}
+			flushOperation(parseInt(userInput));
+			previousOperator = null;
+			userInput = +calcTotal;
+			calcTotal = 0;
+			break;
+
+		case "←":
+			if (userInput.length === 1) {
+				userInput = "0";
+			} else {
+				userInput = userInput.substring(0, userInput.length - 1);
+			}
+			break;
+
+		case "+":
+		case "-":
+		case "×":
+		case "÷":
+			handleMath(value);
+			break;
+	}
+}
+/*
+	Step 5, if the userInput is equal to zero return, if not
+	send it flushOperation() function
+ */
+function handleMath(value) {
+	if (userInput === "0") {
+		return;
+	}
+
+	const intuserInput = parseInt(userInput);
+
+	if (calcTotal === 0) {
+		calcTotal = intuserInput;
 	} else {
-		 operatorFunc(btn)
-		}
-	})
+		flushOperation(intuserInput);
+	}
+	previousOperator = value;
+
+	userInput = "0";
+
+}
+/*
+	Step 6 Depending on the button/symbol they clicked, it will do the corresponding math.
+*/
+function flushOperation(intuserInput) {
+	if (previousOperator === "+") {
+		calcTotal += intuserInput;
+	} else if (previousOperator === "-") {
+		calcTotal -= intuserInput;
+	} else if (previousOperator === "×") {
+		calcTotal *= intuserInput;
+	} else {
+		calcTotal /= intuserInput;
+	}
+}
+
+
